0d2257da37fa82d0033e9c44b530fdd62e99d7cb
carsonrp <carsonrp@uw.edu>
2026-01-14 15:58:37 -0800

    up to 5:27

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
 const gameEngine = new GameEngine();
 
 const ASSET_MANAGER = new AssetManager();
+ASSET_MANAGER.queueDownload("./sailormars.png");
 
 ASSET_MANAGER.downloadAll(() => {
 	const canvas = document.getElementById("gameWorld");
 	const ctx = canvas.getContext("2d");
+
+gameEngine.addEntity(new sailormars());
 
 	gameEngine.init(ctx);
 
---
+++ sailormars.js
@@ -0,0 +1,12 @@
+class sailormars {
+	constructor() {
+
+	};
+	update() {
+
+	};
+	draw(ctx) {
+		ctx.drawImage(ASSET_MANAGER.getAsset("./sailormars.png"),0,0);
+
+	};
+}
