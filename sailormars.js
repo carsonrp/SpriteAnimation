480b33797f114226b363d4266fdafb998a615a5c
carsonrp <carsonrp@uw.edu>
2026-01-16 19:22:16 -0800

    fixed walked out of frame

--- sailormars.js
+++ sailormars.js
@@ -2,7 +2,7 @@ class sailormars {
 	constructor(game) {
 		this.game = game;
 		this.animator = new Animator(ASSET_MANAGER.getAsset("./sailormars.png"), 
-		0, 0, 41, 108, 12, 0.2);
+		0, 0, 45, 108, 12, 0.2);
 
 	};
 	update() {
